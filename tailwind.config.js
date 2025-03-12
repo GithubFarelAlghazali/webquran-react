729a82d7c7bbbe11ca3af54f55b9cfe348d61cf7
Farel Alghazali <138081164+GithubFarelAlghazali@users.noreply.github.com>
2025-03-12 11:45:55 +0700

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
      content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+     darkMode: "class",
      theme: {
           extend: {
                fontFamily: {
                     lato: ["Lato", "sans-serif"],
                     quicksand: ["Quicksand", "sans-serif"],
                     amiri: ["Amiri Quran", "serif"],
-                    ayat: ["Ayat Quran 1", "serif"],
+                    uthmani: ["uthmani", "serif"],
                },
           },
      },
